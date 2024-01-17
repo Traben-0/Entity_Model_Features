433ff0c819c8be0efe386034b03b7a5123545e5c
Traben <ben.trabant@gmail.com>
2024-01-17 15:54:57 +1000

    forge launch fix

--- forge/src/main/java/traben/entity_model_features/forge/EMFVersionDifferenceManagerImpl.java
+++ forge/src/main/java/traben/entity_model_features/forge/EMFVersionDifferenceManagerImpl.java
@@ -1,7 +1,11 @@
 package traben.entity_model_features.forge;
 
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.loading.LoadingModList;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 
@@ -15,7 +19,26 @@ public class EMFVersionDifferenceManagerImpl {
 
 
     public static boolean isThisModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
+        try {
+            ModList list = ModList.get();
+            if (list == null) {
+                LoadingModList list2 = FMLLoader.getLoadingModList();
+                return list2 != null && checkInitialModList(list2, modId);
+            }
+            return list.isLoaded(modId);
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private static boolean checkInitialModList(@NotNull LoadingModList list, String modId){
+        try {
+            for (ModInfo mod : list.getMods()) {
+                if(mod.getModId().equals(modId)) return true;
+            }
+        }catch (Exception e){
+        }
+        return false;
     }
 
 
