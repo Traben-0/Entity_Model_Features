4277a775522ecf0a78645e7dbeadd42f693867e8
Traben <ben.trabant@gmail.com>
2024-01-17 15:23:08 +1000

    forge launch fix

--- neoforge/src/main/java/traben/entity_model_features/neoforge/EMFVersionDifferenceManagerImpl.java
+++ neoforge/src/main/java/traben/entity_model_features/neoforge/EMFVersionDifferenceManagerImpl.java
@@ -2,7 +2,11 @@ package traben.entity_model_features.neoforge;
 
 
 import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.fml.loading.LoadingModList;
+import net.neoforged.fml.loading.moddiscovery.ModInfo;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 
@@ -16,7 +20,26 @@ public class EMFVersionDifferenceManagerImpl {
 
 
     public static boolean isThisModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
+        try {
+            ModList list = ModList.get();
+            if (list == null) {
+                LoadingModList list2 = FMLLoader.getLoadingModList();
+                return list2 != null && checkInitialModList(list2, modId);
+            }
+            return list.isLoaded(modId);
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private static boolean checkInitialModList(@NotNull LoadingModList list, String modId){
+        try {
+            for (ModInfo mod : list.getMods()) {
+                if(mod.getModId().equals(modId)) return true;
+            }
+        }catch (Exception e){
+        }
+        return false;
     }
 
 
