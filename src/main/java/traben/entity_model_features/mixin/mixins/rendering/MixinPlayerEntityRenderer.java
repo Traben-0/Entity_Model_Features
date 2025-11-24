559b61fbd53a2071d519fae18627593ce225edfa
Traben <ben.trabant@gmail.com>
2025-11-24 22:05:49 +1000

    cleanup

--- src/main/java/traben/entity_model_features/mixin/mixins/rendering/MixinPlayerEntityRenderer.java
+++ src/main/java/traben/entity_model_features/mixin/mixins/rendering/MixinPlayerEntityRenderer.java
@@ -1,11 +1,8 @@
 package traben.entity_model_features.mixin.mixins.rendering;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.SubmitNodeCollector;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
