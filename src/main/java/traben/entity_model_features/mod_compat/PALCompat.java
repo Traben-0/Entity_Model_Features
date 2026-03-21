1b7a31ff1702b86c00cfa1473629916c2cccc1d2
snakehugo <hugo.garde@epita.fr>
2026-03-21 18:59:48 +0100

    - Moved EmoteCraft compatibility in PALCompat.java - Added compatibility for 1.20.1 => EmoteCraft uses playeranimator instead of PAL

--- src/main/java/traben/entity_model_features/mod_compat/PALCompat.java
+++ src/main/java/traben/entity_model_features/mod_compat/PALCompat.java
@@ -9,9 +9,19 @@ import com.zigythebird.playeranim.animation.AvatarAnimManager;
 //$$ import com.zigythebird.playeranim.accessors.IAnimatedPlayer;
 //$$ import com.zigythebird.playeranim.animation.PlayerAnimManager;
 //#endif
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 import traben.entity_model_features.utils.EMFEntity;
+import traben.entity_model_features.utils.EMFUtils;
+
+import java.lang.reflect.Method;
 
 public class PALCompat {
+
+    private static boolean checkedIfIEmotePlayerExists = false;
+    private static Class<?> iEmotePlayerEntityType = null;
+    private static Method isPlayingEmoteMethod = null;
+
     public static boolean shouldPauseEntityAnim(EMFEntity entity) {
         //#if FORGE || MC <12100
         //#elseif MC>=12109
@@ -25,6 +35,61 @@ public class PALCompat {
         //$$     return manager != null && manager.isActive();
         //$$ }
         //#endif
-        return false;
+
+        //EMFUtils.log("Emoting: " + isPlayerEmoting(entity));
+        // When emoting with EMOTECRAFT mod, the player will be forced to his vanilla model to emote properly
+        return isPlayerEmoting(entity);
     }
+
+    // ---------------------------------------------------------------------------------
+    // ------------------------ EMOTECRAFT comptability section ------------------------
+
+    public static boolean isPlayerEmoting(EMFEntity entity) {
+        if (!(entity instanceof Player player)) return false;
+
+        Method emoteMethod = getIsPlayingEmoteMethod();
+        if (emoteMethod == null) return false;
+
+        try {
+            return (boolean) emoteMethod.invoke(player);
+        } catch (Exception ignored) {
+            return false;
+        }
+    }
+
+    private static @Nullable Class<?> getIEmotePlayerEntityType() {
+        if (checkedIfIEmotePlayerExists) return iEmotePlayerEntityType;
+        checkedIfIEmotePlayerExists = true;
+
+        try {
+            // Tries to get the IEmotePlayerEntity interface in order to access the isPlayingEmote() method
+            // https://github.com/KosmX/emotes/blob/1.20.1/executor/src/main/java/io/github/kosmx/emotes/executor/emotePlayer/IEmotePlayerEntity.java
+            // This type should always be found if EmoteCraft mod doesn't change it too much and the mod is actually loaded obv
+            iEmotePlayerEntityType = Class.forName("io.github.kosmx.emotes.executor.emotePlayer.IEmotePlayerEntity");
+        } catch (ClassNotFoundException ignored) {
+            iEmotePlayerEntityType = null;
+        }
+
+        return iEmotePlayerEntityType;
+    }
+
+    private static @Nullable Method getIsPlayingEmoteMethod() {
+        if (isPlayingEmoteMethod != null) return isPlayingEmoteMethod;
+
+        Class<?> emotePlayerType = getIEmotePlayerEntityType();
+        if (emotePlayerType == null) return null;
+
+        try {
+            isPlayingEmoteMethod = emotePlayerType.getMethod("isPlayingEmote");
+        } catch (NoSuchMethodException ignored) {
+            isPlayingEmoteMethod = null;
+        }
+
+        return isPlayingEmoteMethod;
+    }
+
+    // -------------------- end of EMOTECRAFT comptability section ---------------------
+    // ---------------------------------------------------------------------------------
+
+
 }
